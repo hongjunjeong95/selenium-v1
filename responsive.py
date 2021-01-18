0e08194d7b1a8e654ab05a74973b8b1cb96937d0
zpskek <wjdghdwns0@gmail.com>
2021-01-18 19:20:49 +0900

    Refactoring Responsive tester

--- responsive.py
+++ responsive.py
@@ -3,21 +3,36 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-BROWSER_HEIGHT = 1027
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
+class ResponsiveTester:
+    def __init__(self, urls):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+        self.urls = urls
+        self.sizes = [480, 960, 1366, 1920]
 
-browser.get("https://nomadcoders.co")
-browser.maximize_window()
+    def screenshot(self):
+        BROWSER_HEIGHT = 1027
 
-sizes = [480, 960, 1366, 1920]
+        for size in self.sizes:
+            self.browser.set_window_size(size, BROWSER_HEIGHT)
+            self.browser.execute_script("window.scrollTo(0, 0)")
+            time.sleep(3)
+            scroll_size = self.browser.execute_script(
+                "return document.body.scrollHeight"
+            )
+            total_sections = ceil(scroll_size / BROWSER_HEIGHT)
+            for section in range(total_sections + 1):
+                self.browser.execute_script(
+                    f"window.scrollTo(0, {section * BROWSER_HEIGHT})"
+                )
+                self.browser.save_screenshot(f"screenshots/{size}x{section}.png")
 
-for size in sizes:
-    browser.set_window_size(size, BROWSER_HEIGHT)
-    browser.execute_script("window.scrollTo(0, 0)")
-    time.sleep(3)
-    scroll_size = browser.execute_script("return document.body.scrollHeight")
-    total_sections = ceil(scroll_size / BROWSER_HEIGHT)
-    for section in range(total_sections + 1):
-        browser.execute_script(f"window.scrollTo(0, {section * BROWSER_HEIGHT})")
-        browser.save_screenshot(f"screenshots/{size}x{section}.png")
+    def start(self):
+        for url in self.urls:
+            self.browser.get(url)
+            self.browser.maximize_window()
+            self.screenshot()
+
+
+tester = ResponsiveTester(["https://nomadcoders.co", "https://google.com"])
+tester.start()
