ab0a10cd0a2183bed7856601a2b9e1001765b9c8
zpskek <wjdghdwns0@gmail.com>
2021-01-19 13:16:41 +0900

    Counting posts

---
+++ nomad_instagraming.py
@@ -0,0 +1,46 @@
+import time
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+browser = webdriver.Chrome(ChromeDriverManager().install())
+
+
+def wait_for(locator):
+    return WebDriverWait(browser, 10).until(EC.presence_of_element_located(locator))
+
+
+main_hashtag = "dog"
+
+browser.get(f"https://www.instagram.com/explore/tags/{main_hashtag}")
+
+header = wait_for((By.TAG_NAME, "header"))
+
+hashtags = header.find_elements_by_class_name("AC7dP")
+
+for hashtag in hashtags:
+    ActionChains(browser).key_down(Keys.COMMAND).click(hashtag).perform()
+
+
+counted_hashtags = []
+used_hashtags = []
+
+for window in browser.window_handles:
+    browser.switch_to.window(window)
+    hashtag_name = wait_for((By.TAG_NAME, "h1"))
+    post_count = wait_for((By.CLASS_NAME, "g47SY"))
+    if post_count:
+        post_count = int(post_count.text.replace(",", ""))
+    if hashtag_name:
+        hashtag_name = hashtag_name.text[1:]
+    if hashtag_name and post_count:
+        if hashtag_name not in used_hashtags:
+            counted_hashtags.append((hashtag_name, post_count))
+            used_hashtags.append(hashtag_name)
+
+time.sleep(3)
+browser.quit()
